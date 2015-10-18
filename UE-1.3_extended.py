700dd08902520c1b2ebe1d17b20a40823c97e27e
Damanu <e.schwarzhans@gmx.at>
2015-10-19 00:07:51 +0200

    did some homework!

---
+++ UE-1.3_extended.py
@@ -0,0 +1,111 @@
+#usr/bin/python!
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt 
+
+PATH="./"
+FILE="data"
+
+#----------subroutines--------------
+def Read():
+	data=[]		#data equals my fouriercoeffitients fi
+	try:
+		text = open(PATH+FILE,'r')	#open file to read
+		data=text.read().splitlines()
+		text.close()
+		return data		
+	except:
+		print 'Something went wrong at %argv[0] subprogram Read() while reading the datafile'
+		sys.exit()
+def Write(data):
+	text = open(PATH+"FT_data",'w+')	#open filen to write
+	for x in data:
+		text.write(str(x)+"\n")	#write data to file 
+	text.close()
+
+def FT(data):
+	N=len(data)
+	print "N"
+	print N
+	fn=[]
+	map(complex,fn)
+	n=-N/2
+	i=-N/2
+	while n<=N/2-1:
+#		print "n=",n+N/2
+		fn.append(0.0j)
+		for fi in data:
+			fn[n+N/2]+=1.0*complex(np.exp(-2.0*np.pi*n*1.0j*i/(N)))*fi
+#			print i
+			i+=1
+		n+=1
+		i=-N/2
+	return fn
+	
+def IFT(data):
+	N=len(data)
+	print "N"
+	print N
+	fn=[]
+	map(complex,fn)
+	n=-N/2
+	i=-N/2
+	while n<=(N/2-1):
+#		print "n=",n+N/2
+		fn.append(0.0)
+		for fi in data:
+			fn[n+N/2]+=1.0/N*np.exp(2.0*np.pi*n*1.0j*i/N)*fi
+#			print i
+			i+=1
+		n+=1
+		i=-N/2
+	return fn
+
+
+#--------------Main Program---------------
+def main():
+#	fi=Read()
+#	fi[:]=np.linspace(0,1,100)
+	N=1000
+	fi=[]
+	n=-N/2
+#	while n<=N/2-1:
+#		fi.append(1.0j)
+#		if n==0:
+#			fi[n+N/2]=0.0 + 0.0j
+#		elif n!=0:
+#			fi[n+N/2]=1.0j/2.0*(N/n)**3*(complex(np.cos(n*1.0/N))-1.0)	
+#		n+=1
+	while n<=N/2-1:
+		fi.append(1.0j)
+		if n*2./N<-1./2:
+			fi[n+N/2]=0
+		if n*2./N>=-1./2 and n*2./N<=0:
+			fi[n+N/2]=-1
+		if n*2./N>=0 and n*2./N<=1./2:
+			fi[n+N/2]=1
+		if n*2./N>1./2:
+			fi[n+N/2]=0
+		n+=1
+#	N=len(fi)
+	fi=map(complex,fi)
+	plotfi1=plt.plot(np.linspace(-1,1,N),fi)
+#	print "fi", fi
+	fn=FT(fi)
+	i=0
+	for x in fn:
+		fn[i]=x.imag	
+		i+=1
+
+#	Write(fn)
+#	print "fn",fn
+#	fi=FT(fn)
+#	print "fi",fi
+	plotfn=plt.plot(np.linspace(-1,1,N),fn)
+#	plotfi=plt.plot(np.linspace(0,1,N),fi)
+#	fn=FT(fi)
+#	plotfn2=plt.plot(np.linspace(0,1,N),fn)
+	plt.show()
+if __name__=="__main__":
+	main()
