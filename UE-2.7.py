1621e0da9bea4419785a48453779cd0e41429d9c
Damanu <e.schwarzhans@gmx.at>
2015-11-11 20:51:07 +0100

    ...

--- UE-2.7.py
+++ UE-2.7.py
@@ -63,19 +63,18 @@ def leapfrog(LU,U,dt,i):
 	return ((U[i-1]+2.*dt*LU[i]))
 
 def main():
-	T=100.
-	N=10000
+	T=10.
+	N=1000
 	dt=T/N
 	print dt
 	xaxe=np.linspace(0,T,N)
 	v=[0.,-np.sin(dt)]
-	x=[1.,-np.cos(dt)]
+	x=[1.,np.cos(dt)]
 	i=1	
 	ddt=0.
 	while i<N-1:
 	#	v.append(eu_int_impl(v[i],-x[i],dt))
 	#	x.append(eu_int_fw(x[i], v[i+1],dt))
-
 		v.append(leapfrog(v,x,-dt,i))
 		x.append(leapfrog(x,v,dt,i))
 		i+=1
