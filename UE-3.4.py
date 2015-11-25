1234713f4768518adf79e4eff92f322836590df5
Damanu <e.schwarzhans@gmx.at>
2015-11-25 23:55:11 +0100

    ad

--- UE-3.4.py
+++ UE-3.4.py
@@ -16,7 +16,10 @@ def fzd(func,dx):	#functions starts at second point (so fx[0]=fx_real[1])
 	return fx
 
 def function(t,x):
-	return(1/2.*(1-np.exp(-4.*np.pi**2*t)*np.cos(2.*np.pi*x)))
+	return(list(1/2.*(-np.exp(-4.*np.pi**2*t)*np.cos(2.*np.pi*x))))
+
+def zdiff(u,j,n,dx):
+	return(1./dx**2*(u[n][j+1]-2*u[n][j]+u[n][j-1]))
 
 def main():
 	u=[[]]
@@ -24,37 +27,32 @@ def main():
 	X=1.	#spaceinterval
 	NX=100
 	dx=X/NX
-	dt=dx**2/6.
+#	dt=dx**2/6.
+	dt=0.00001
+	print dt
 #	dx=np.sqrt(dt*6)
 	D=1.
 	NT=int(T/dt)
 #	NX=int(X/dx)
 	j=0
 	n=0
-	print NT
-	print NX
 	while j<NX:
 		u[0].append(1/2.*(1-np.cos(2*np.pi*dx*j)))	# index 0 is time, 1 is position
 		j+=1
 	n=0
-	print len(u[0])
 	while n<NT: #
 		j=1
-		print u[n]
 		u.append([u[n][0]])	#lower boundary
 		while j<NX-1:
-			u[n+1].append(u[n][j]+D*dt/dx**2*(u[n][j+1]-2*u[n][j]+u[n][j-1]))
+			u[n+1].append(u[n][j]+D*dt*zdiff(u,j,n,dx))
 			j+=1
-			print j
-		u[n+1].append(u[n][NX])	#upper boundary
-		print "u len: ",len(u[n+1])
+		u[n+1].append(u[n][NX-1])	#upper boundary
 		n+=1
 	xaxe=np.linspace(0,X,len(u[NT-1]))
-	print u
 	taxe=np.linspace(0,T,len(u[0]))
-	plt.plot(xaxe,u[0])
-	plt.plot(xaxe,u[NT-1])
-	plt.plot(xaxe,function(0.1,xaxe))
+	plt.plot(xaxe,u[0],color='b')
+	plt.plot(xaxe,u[NT-1],color='g')
+	plt.plot(xaxe,function(0.05,xaxe),color='y')
 	plt.show()
 if __name__=="__main__":
 	main()
---
+++ UE-3.5.py
@@ -0,0 +1,38 @@
+#/usr/bin/python!
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+def disp(dt,dx,v,k):
+	w=np.log(1-1.0j*v*dt/dx*np.sin(k*dx))/(1.0j*dt)
+	return w
+
+def disp_lax(dt,dx,v,k):
+	w=np.log(np.cos(k*dx)-1.0j*v*dt/dx*np.sin(k*dx))/(1.0j*dt)
+	return w
+
+def disp_analyt(v,k):
+	w=v*k
+	return w
+
+def main():
+	v=1.
+	dx=1.
+	dt=1.
+	k=np.linspace(0,np.pi,1000)
+	v=0.25
+	while v<=1:	
+		a=v*dt/dx
+		w=disp(dt,dx,v,k)*dt/a
+		w_=disp_analyt(v,k)*dt/a
+		plt.subplot(1,2,1)
+		plt.plot(k,abs(w.real),color='r')
+		plt.plot(k,disp_analyt(v,k),color='b')
+		plt.subplot(1,2,2)
+		plt.plot(k,abs(w.imag))
+		plt.plot(k,abs(disp_analyt(v,k).imag))
+		v+=0.25
+	plt.show()
+if __name__=="__main__":
+	main()
